9b02c5db61f4200a2d578aab0fcc54ed036d0c08
Prince4636 <112784384+Prince4636@users.noreply.github.com>
2022-09-04 04:42:43 +0530

    Update __init__.py

--- YukkiMusic/__init__.py
+++ YukkiMusic/__init__.py
@@ -3,7 +3,7 @@
 #
 # This file is part of < https://github.com/YukkiMusic/YukkiMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/Prince4636/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/YukkiMusic/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
 
